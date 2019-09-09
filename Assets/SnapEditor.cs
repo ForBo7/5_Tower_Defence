98aff1c87007af944fcb27a7352ccd822efa6c06
Salman Naqvi <salmananaqvii+atlassianbitbucket@gmail.com>
2019-09-09 10:29:21 +0500

    Created a snap editor script.

---
+++ Assets/SnapEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] int gridSize;
+
+    void Update()
+    {
+        float gridSizeF = (float)gridSize;
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSizeF) * gridSizeF;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSizeF) * gridSizeF;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
